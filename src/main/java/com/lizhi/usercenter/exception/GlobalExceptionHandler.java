6bf1a7a3a98498f2d514a01697c7de9252479090
lizhi <2318691019@qq.com>
2023-04-12 00:30:14 +0800

    Initial commit

---
+++ src/main/java/com/lizhi/usercenter/exception/GlobalExceptionHandler.java
@@ -0,0 +1,30 @@
+package com.lizhi.usercenter.exception;
+
+import com.lizhi.usercenter.common.BaseResponse;
+import com.lizhi.usercenter.common.ErrorCode;
+import com.lizhi.usercenter.common.ResultUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+/**
+ * 全局异常处理器
+ *
+ * @author 荔枝
+ */
+@RestControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(BusinessException.class)
+    public BaseResponse businessExceptionHandler(BusinessException e) {
+        log.error("businessException: " + e.getMessage(), e);
+        return ResultUtils.error(e.getCode(), e.getMessage(), e.getDescription());
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public BaseResponse runtimeExceptionHandler(RuntimeException e) {
+        log.error("runtimeException", e);
+        return ResultUtils.error(ErrorCode.SYSTEM_ERROR, e.getMessage(), "");
+    }
+}
---
+++ src/main/java/com/lizhi/usercenter/mapper/UserMapper.java
@@ -0,0 +1,15 @@
+package com.lizhi.usercenter.mapper;
+
+import com.lizhi.usercenter.model.domain.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * @Entity com.lizhi.usercenter.model.domain.User
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
